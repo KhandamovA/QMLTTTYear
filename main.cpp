c316ee8ea70d49db345588863ecf3bad30672944
KhandamovA <77474720+KhandamovA@users.noreply.github.com>
2026-04-27 16:16:27 +0500

    перенес сцену

--- main.cpp
+++ main.cpp
@@ -18,19 +18,25 @@ int main(int argc, char *argv[])
     if_.group = "Управление";
     watcher->registerBlock(if_);
 
-    BlockData test;
-    test.type = 1;
-    test.viewTexts = {"Вернуть $$"};
-    test.blockShape = 1;
-    test.group = "Репортеры";
-    test.bodyColor = "cyan";
-    watcher->registerBlock(test);
+    BlockData if2_;
+    if2_.type = 1;
+    if2_.viewTexts = {"Если $$ тогда", ""};
+    if2_.group = "Управление";
+    watcher->registerBlock(if2_);
 
     BlockData while_;
     while_.type = 2;
     while_.viewTexts = {"Повторять пока $$", ""};
     while_.group = "Управление";
     watcher->registerBlock(while_);
+
+    BlockData test;
+    test.type = 3;
+    test.viewTexts = {"Вернуть $$"};
+    test.blockShape = 1;
+    test.group = "Репортеры";
+    test.bodyColor = "cyan";
+    watcher->registerBlock(test);
 
     return app.exec();
 }
